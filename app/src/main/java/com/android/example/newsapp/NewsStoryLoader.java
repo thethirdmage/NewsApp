23c5da5038333b37f5b8565961f3419ef932e58b
Selene Marivaldi <bobdablob42@gmail.com>
2018-06-27 08:28:43 -0400

    Add news story loader

---
+++ app/src/main/java/com/android/example/newsapp/NewsStoryLoader.java
@@ -0,0 +1,30 @@
+package com.android.example.newsapp;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v4.content.AsyncTaskLoader;
+import java.util.ArrayList;
+
+public class NewsStoryLoader extends AsyncTaskLoader<ArrayList<NewsStory>> {
+
+    private String mUrl;
+
+    public NewsStoryLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+    }
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    @Nullable
+    @Override
+    public ArrayList<NewsStory> loadInBackground() {
+        if (mUrl == null) {
+            return null;
+        }
+
+        return QueryUtils.extractNewsStories(mUrl);
+    }
+}
